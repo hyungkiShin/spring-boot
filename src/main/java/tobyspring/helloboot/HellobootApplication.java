939164917f405da091920fe6aa47ad9a11532209
hyungkiShin <tkaqkeldk@nate.com>
2023-01-23 22:32:27 +0900

    feat: 서블릿 요청 처리

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,16 +2,32 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        // TomcatServletWebServer 를 만드는데 복잡한 생성과정과 복잡한 설정등을 지원하고, 모든 설정을 마친뒤에 tomcatServletWebserver 를 생성해 달라는 요청을 하면, 우아하게 만들어주는 일종의 도우미 클래스
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-        // servletContainer 를 만드는 함수 ( 이게 진짜 WebServer )
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext ->
+                servletContext.addServlet("hello", new HttpServlet() {
+                    @Override
+                    protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+                        String name = req.getParameter("name");
+
+                        resp.setStatus(HttpStatus.OK.value()); // 상태코드
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Header
+                        resp.getWriter().println("Hello " + name); // body
+                    }
+                }).addMapping("/hello"));
         webServer.start();
     }
-
 }
