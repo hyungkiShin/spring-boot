7d58a0e13ccfc1da52cda70f4b9b2ed7cb8b5175
hyungkiShin <tkaqkeldk@nate.com>
2023-01-24 19:09:52 +0900

    feat: dependency injection

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,11 +3,15 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return "hello " + name;
+        SimpleHelloService simpleHelloService = new SimpleHelloService();
+
+        return simpleHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
