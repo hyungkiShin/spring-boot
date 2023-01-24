338689eeb7041ae1f657021f2d93f89eaf5273a7
hyungkiShin <tkaqkeldk@nate.com>
2023-01-24 11:58:16 +0900

    docs: 프론트 컨트롤러로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,16 +19,24 @@ public class HellobootApplication {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
         WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("hello", new HttpServlet() {
+                servletContext.addServlet("frontcontroller", new HttpServlet() {
                     @Override
                     protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-                        String name = req.getParameter("name");
+                        // 인증, 보안, 다국어, 공통 기능 한다고 가정
+                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 
-                        resp.setStatus(HttpStatus.OK.value()); // 상태코드
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Header
-                        resp.getWriter().println("Hello " + name); // body
+                            String name = req.getParameter("name");
+
+                            resp.setStatus(HttpStatus.OK.value()); // 상태코드
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Header
+                            resp.getWriter().println("Hello " + name); // body
+                        } else if (req.getRequestURI().equals("/user")) {
+                            //
+                        } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+                        }
                     }
-                }).addMapping("/hello"));
+                }).addMapping("/*"));
         webServer.start();
     }
 }
