201c6ed4b47927471daa9acc0c6cb8cf10c354e6
hyungkiShin <tkaqkeldk@nate.com>
2023-01-27 00:28:52 +0900

    feat: 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,17 +9,24 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+
+            }
+        };
+
         applicationContext.registerBean(HelloController.class); // bean 등록
         applicationContext.registerBean(SimpleHelloService.class); // bean 등록
         applicationContext.refresh(); // bean object 를 만들어준다
-
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                        new DispatcherServlet(applicationContext)
-                    ).addMapping("/*");
-        });
-        webServer.start();
     }
 }
